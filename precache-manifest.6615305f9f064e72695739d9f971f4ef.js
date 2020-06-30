411aa7fc98e7d263ac6c86826390749a76288b99
cognitive-ninja <saurabhhirugade@gmail.com>
2020-06-30 09:33:42 +0530

    Updates

---
+++ precache-manifest.6615305f9f064e72695739d9f971f4ef.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a39a353a3b149c04eb1d73144d99a6e",
+    "url": "/any-event/index.html"
+  },
+  {
+    "revision": "9681d63b64d14b715e5c",
+    "url": "/any-event/static/css/2.bf5b17f7.chunk.css"
+  },
+  {
+    "revision": "1db681469f9d2a12aa74",
+    "url": "/any-event/static/css/main.4202595f.chunk.css"
+  },
+  {
+    "revision": "9681d63b64d14b715e5c",
+    "url": "/any-event/static/js/2.0f52f69a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/any-event/static/js/2.0f52f69a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1db681469f9d2a12aa74",
+    "url": "/any-event/static/js/main.42d310e8.chunk.js"
+  },
+  {
+    "revision": "d958ad1a36ca5d1a0eb5",
+    "url": "/any-event/static/js/runtime-main.ee985e05.js"
+  },
+  {
+    "revision": "16d8d417e88acdf21e77e8087952fd9a",
+    "url": "/any-event/static/media/backg-01.16d8d417.png"
+  }
+]);
