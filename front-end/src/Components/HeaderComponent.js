aecbfb50571c0c055fae6b5614495acb6bfe2cc6
abhishek-g-joshi <ajabhishekgjoshi@gmail.com>
2020-07-08 12:23:59 +0530

    Date Of Birth added in Registration.Upadate the EventComponent

--- front-end/src/Components/HeaderComponent.js
+++ front-end/src/Components/HeaderComponent.js
@@ -1,53 +1,61 @@
-import React, { Component } from 'react';
-import {Nav,Navbar, NavbarBrand, Collapse, NavItem, NavLink, NavbarToggler} from 'reactstrap';
-import {Link} from 'react-router-dom';
- import '../App.css';
+import React, { Component } from "react";
+import {
+  Nav,
+  Navbar,
+  NavbarBrand,
+  Collapse,
+  NavItem,
+  NavbarToggler,
+} from "reactstrap";
+import { Link } from "react-router-dom";
+import "../App.css";
 
 class Header extends Component {
-    constructor(props){
-        super(props);
-        this.state={
-            isNavOpen:false,
-            isModalOpen:false,
-        }
-        this.toggleNav = this.toggleNav.bind(this);
-        // this.toggleModal = this.toggleModal.bind(this);
-        // this.handleLogin= this.handleLogin.bind(this);
-    }
-    toggleNav(){
-        this.setState({
-            isNavOpen:!this.state.isNavOpen
-        })
-    }
-    render() {
-        return (
-                <>
-                <Navbar dark className='navbar'expand='md'>
-                    <div className="container">
-                    <Link to='/any-event/'>
-                        <NavbarBrand>AnyEvent</NavbarBrand>
-                    </Link>
-                    <NavbarToggler onClick={this.toggleNav}/>
-                    <Collapse isOpen={this.state.isNavOpen} navbar>
-                        <Nav className="ml-auto" navbar>
-                            <NavItem>
-                                <Link className="nav-link" to='/any-event/login'>
-                                    <button type="button" class="btn btn-outline-warning">Log In</button>
-                                </Link>
-                            </NavItem>
-                            <NavItem>
-                                <Link className="nav-link" to='/any-event/register'>
-                                    <button type="button" class="btn btn-outline-warning">Sign Up</button>
-                                </Link>
-                            </NavItem>
-                        </Nav>  
-                    </Collapse>
-                    </div>
-                </Navbar>
-            </>
-        )
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      isNavOpen: false,
+      isModalOpen: false,
+    };
+    this.toggleNav = this.toggleNav.bind(this);
+  }
+  toggleNav() {
+    this.setState({
+      isNavOpen: !this.state.isNavOpen,
+    });
+  }
+  render() {
+    return (
+      <>
+        <Navbar dark className="navbar" expand="md">
+          <div className="container">
+            <Link to="/any-event/">
+              <NavbarBrand>AnyEvent</NavbarBrand>
+            </Link>
+            <NavbarToggler onClick={this.toggleNav} />
+            <Collapse isOpen={this.state.isNavOpen} navbar>
+              <Nav className="ml-auto" navbar>
+                <NavItem>
+                  <Link className="nav-link" to="/any-event/login">
+                    <button type="button" class="btn btn-outline-warning">
+                      Log In
+                    </button>
+                  </Link>
+                </NavItem>
+                <NavItem>
+                  <Link className="nav-link" to="/any-event/register">
+                    <button type="button" class="btn btn-outline-warning">
+                      Sign Up
+                    </button>
+                  </Link>
+                </NavItem>
+              </Nav>
+            </Collapse>
+          </div>
+        </Navbar>
+      </>
+    );
+  }
 }
 
 export default Header;
-
--- front-end/src/Components/MainComponent.js
+++ front-end/src/Components/MainComponent.js
@@ -1,38 +1,41 @@
-import React, { Component } from 'react';
-import {BrowserRouter as Router,Route} from 'react-router-dom';
-import Header from './HeaderComponent';
-import Footer from './FooterComponent';
-import HomeComponent from './HomeComponent'
-import EVENTS from '../shared/events';
-import Register from './auth/Register';
-import Login from './auth/Login';
-import EventComponent from './EventComponent';
-
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Header from "./HeaderComponent";
+import Footer from "./FooterComponent";
+import HomeComponent from "./HomeComponent";
+import EVENTS from "../shared/events";
+import Register from "./auth/Register";
+import Login from "./auth/Login";
+import EventComponent from "./EventComponent";
 
 class MainComponent extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            events:EVENTS
-        }
-    }
-    render() {
-        return (
-            <div>
-                <Router>
-                    <Header/>  
-                    <Route exact path="/any-event/" component={HomeComponent}/>           
-                    <div className="container">
-                        <Route exact path='/any-event/register' component={Register}/>
-                        <Route exact path='/any-event/login' component={Login}/>
-                        {/* <Route exact path='/any-event/all-events' component={()=> <EventComponent events={this.state.events}/>}/>  */}
-                    </div> 
-                    <Route exact path='/any-event/all-events' component={()=> <EventComponent events={this.state.events}/>}/> 
-                    <Footer/>
-                </Router>
-            </div>
-        )
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      events: EVENTS,
+    };
+  }
+  render() {
+    return (
+      <div>
+        <Router>
+          <Header />
+          <Route exact path="/any-event/" component={HomeComponent} />
+          <div className="container">
+            <Route exact path="/any-event/register" component={Register} />
+            <Route exact path="/any-event/login" component={Login} />
+            {/* <Route exact path='/any-event/all-events' component={()=> <EventComponent events={this.state.events}/>}/>  */}
+          </div>
+          <Route
+            exact
+            path="/any-event/all-events"
+            component={() => <EventComponent events={this.state.events} />}
+          />
+          <Footer />
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default MainComponent;
